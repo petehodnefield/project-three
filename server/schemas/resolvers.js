650788ddfe39f27b4c35bbb787796ac9f33eaa4a
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-08-02 09:39:25 -0500

    added express middleware to get server running Added basic User model and resolvers/typeDefs just to get things working

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,12 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User} = require('../models');
+
+const resolvers = {
+    Query: {
+        users: async () => {
+            return await User.find()
+        }
+    }
+}
+
+module.exports = resolvers;
