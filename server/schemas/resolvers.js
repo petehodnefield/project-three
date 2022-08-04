b6e7f5453feb1ab51ddfedca903f84b23565b374
brynnke <100329799+brynnke@users.noreply.github.com>
2022-08-03 20:07:59 -0500

    adding reaction model

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,7 +7,21 @@ const resolvers = {
         users: async () => {
             return await User.find()
         },
-    }
+    },
+    // added reaction to resolvers
+    addReaction: async (parent, { eventId, reactionBody }, context) => {
+        if (context.user) {
+          const updatedEvent = await Event.findOneAndUpdate(
+            { _id: eventId },
+            { $push: { reactions: { reactionBody, username: context.user.username } } },
+            { new: true, runValidators: true }
+          );
+  
+          return updatedEvent;
+        }
+  
+        throw new AuthenticationError('You need to be logged in!');
+      },
 }
 
 
