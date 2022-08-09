3816ceadf66b47dacd8528e8545e44a2e0783ac2
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-08-09 08:47:35 -0500

    large cards printing with event data

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,47 +1,45 @@
-import React from 'react';
-import '../App.css';
-import { useQuery, gql } from '@apollo/client';
+import React from "react";
+import "../App.css";
+import { useQuery, gql } from "@apollo/client";
 // Components
 import Login from "../components/Login";
 import SignUp from "../components/SignUp";
 import LongCard from "../components/LongCard";
 import ShortCard from "../components/ShortCard";
-import ColleaguesList from '../components/ColleaguesList';
-import ColleaguesSearch from '../components/ColleaguesSearch';
-import QuickSearches from '../components/QuickSearches';
-import NavButtons from '../components/NavButtons';
+import ColleaguesList from "../components/ColleaguesList";
+import ColleaguesSearch from "../components/ColleaguesSearch";
+import QuickSearches from "../components/QuickSearches";
+import NavButtons from "../components/NavButtons";
+import auth from "../utils/auth";
 
 const GET_EVENTS = gql`
-query events {
-  events {
-    date
-    name
-    description
-   culture
-    _id
+  query events {
+    events {
+      date
+      name
+      description
+      culture
+      _id
+    }
   }
- }
 `;
 
 function Home() {
-
   const { loading, error, data } = useQuery(GET_EVENTS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :( WOE!!!</p>;
 
-
-
-  function renderLogin(testUserLoggedIn) {
-    if (testUserLoggedIn) {
-      return (<>
-        <Login></Login>
-        <SignUp></SignUp>
-      </>)
-    } else {
-      return <></>
-    }
-  }
+  // function renderLogin(testUserLoggedIn) {
+  //   if (testUserLoggedIn) {
+  //     return (<>
+  //       <Login></Login>
+  //       <SignUp></SignUp>
+  //     </>)
+  //   } else {
+  //     return <></>
+  //   }
+  // }
 
   console.log(data);
 
@@ -50,23 +48,42 @@ function Home() {
       <header>
         <h1 className="white big">DEI CALENDAR</h1>
       </header>
-
-      <main className="columns">
-        <nav className="m-0 p-0 container is-flex is-flex-direction-column is-align-items-center column is-one-quarter">
-          <NavButtons isUserLoggedIn={false} iLoveCake={true}></NavButtons>
-          <QuickSearches></QuickSearches>
-        </nav>
-        <main className="container is-flex is-flex-direction-column is-align-items-center">
-          <h1 className="cream">UPCOMING CULTURAL EVENTS</h1>
-          <LongCard></LongCard>
-          <ShortCard></ShortCard>
-          {renderLogin(false)}
+      {auth.loggedIn() ? (
+        <main className="columns">
+          <nav className="m-0 p-0 container is-flex is-flex-direction-column is-align-items-center column is-one-quarter">
+            <NavButtons></NavButtons>
+            <QuickSearches></QuickSearches>
+          </nav>
+          <main className="container is-flex is-flex-direction-column is-align-items-center">
+            <h1 className="cream">UPCOMING CULTURAL EVENTS</h1>
+            <div>
+              <LongCard></LongCard>
+              <ShortCard></ShortCard>
+              <div>
+                {data.events.map((event) => (
+                  <div className="short-card full-width">
+                    <div className="p-2">
+                      <h1 className="p-0 big white">{event.date}</h1>
+                    </div>
+                    <div className="p-3 is-flex is-flex-direction-column is-align-items-center cream-back">
+                      <h1>{event.name}</h1>
+                      <h3 className="m-2 culture-tag raise">{event.culture}</h3>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </main>
         </main>
-      </main>
+      ) : (
+        <div>
+          <Login></Login>
+          <SignUp></SignUp>
+        </div>
+      )}
     </div>
   );
 }
-
 
 // client
 //   .query({
