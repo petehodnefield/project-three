f140d145a0d7616ad4dab252238cf9542faafe79
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-08-09 08:56:03 -0500

    logout button working

--- client/src/components/NavButtons/index.js
+++ client/src/components/NavButtons/index.js
@@ -1,15 +1,31 @@
 import React from "react";
+import auth from "../../utils/auth";
 
 function NavButtons(props) {
-    return (
+  function logOut() {
+    auth.logout();
+  }
+  return (
+    <div className="is-flex is-flex-direction-column">
+      {!props.isUserLoggedIn ? (
         <div className="is-flex is-flex-direction-column">
-            {!props.isUserLoggedIn ?<button className="m-1 nav-btn" onClick={()=> ""}>HOME</button>:<></>}
-            {props.iLoveCake ?<button className="m-1 nav-btn" onClick={()=> ""}>CAKE</button>:<></>}
-            <button className="m-1 nav-btn" onClick={()=> ""}>DASHBOARD</button>
-            <button className="m-1 nav-btn" onClick={()=> ""}>LOGIN</button>
-            <button className="m-1 nav-btn" onClick={()=> ""}>LOGOUT</button>
-          </div>
-    );
+          <button className="m-1 nav-btn" onClick={() => ""}>
+            HOME
+          </button>
+          <button className="m-1 nav-btn" onClick={() => ""}>
+            DASHBOARD
+          </button>
+          <button className="m-1 nav-btn" onClick={logOut}>
+            LOGOUT
+          </button>
+        </div>
+      ) : (
+        <button className="m-1 nav-btn" onClick={() => ""}>
+          LOGIN
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default NavButtons;
