82279230c0f921aead5ec9e85e70358a7c326529
Omar Wright <98981985+omarwright07@users.noreply.github.com>
2022-08-09 20:09:38 -0500

    started the for each loop for the cultureFilter const in quick searches

--- client/src/components/QuickSearches/index.js
+++ client/src/components/QuickSearches/index.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function QuickSearches() {
-    return (
-      <div className="m-3 is-flex is-flex-direction-column is-align-items-center">
+  const cultureFilter = [
+    'Christian',
+  'Muslim',
+  'Jewish',
+  'Hindu',
+  'Buddhist',
+  'African American',
+  'Asian American',
+  'Mexican American',
+  'Native American',
+  'Special Months'
+];
+
+  return (
+    <div className="m-3 is-flex is-flex-direction-column is-align-items-center full-width">
       <h3>QUICK SEARCHES</h3>
-      <button className="full-width m-1" type=" submit" id="search-btn">
+      {cultureFilter.forEach((element) => (
+                console.log(element)
+                // <Link to={`/events/${element}`}>
+                //   <button className="full-width m-1" type="submit" id="search-btn">{element}
+                //   </button>
+                // </Link>
+              ))}
+      {/* <button className="full-width m-1" type=" submit" id="search-btn">
         American</button>
       <button className="full-width m-1" type=" submit" id="search-btn">
         African American</button>
@@ -13,9 +34,9 @@ function QuickSearches() {
       <button className="full-width m-1" type=" submit" id="search-btn">
         Hispanic/Latino</button>
       <button className="full-width m-1" type=" submit" id="search-btn">
-        Native American</button>
+        Native American</button> */}
     </div>
-    );
+  );
 }
 
 export default QuickSearches;
